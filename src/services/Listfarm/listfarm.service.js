d442c1479254cb00dd99a3f441a2d1225815ebb8
Jayesh Patil <132203403+jayesh-zip@users.noreply.github.com>
2025-07-07 18:18:41 +0530

    add validation in list farm and remove console.log

--- src/services/Listfarm/listfarm.service.js
+++ src/services/Listfarm/listfarm.service.js
@@ -62,11 +62,6 @@ export const submitProperty = async (formData, token) => {
       data.append("property_agreement", formData.property_agreement);
     }
 
-    console.log("📦 Sending form data:");
-    for (let pair of data.entries()) {
-      console.log(`${pair[0]}:`, pair[1]);
-    }
-
     const response = await api.post("/api/add_property", data, {
       headers: {
         Accept: "application/json",
@@ -76,7 +71,6 @@ export const submitProperty = async (formData, token) => {
     });
     return response.data;
   } catch (error) {
-    console.log(error);
     console.error("❌ [submitProperty] Error:", error);
     throw error?.response?.data || error;
   }
